d229961977473254f6470c292ffcdbc56ff89b4f
Victor <victor.shevchuk.96@gmail.com>
2022-09-24 17:34:28 +0300

    data wrangling

--- index.R
+++ index.R
@@ -4,20 +4,39 @@ library(naniar)
 library(ggplot2)
 library(tidyverse)
 
-Star_Wars_characters = read.csv("00_raw_data/SW-characters.csv")
-#glimpse(Star_Wars_characters$birth_year)
-#skimr::skim(Star_Wars_characters)
-sw_clean = janitor::clean_names(Star_Wars_characters)
-sw_clean = sw_clean |> mutate(
-    mass = as.integer(mass), 
-    gender = as.factor(gender))
-#glimpse(sw_clean)
-sw_clean |> 
-  count(gender) |> 
-  ggplot(aes(x = fct_reorder(gender, n, .desc = TRUE),
-             y = n)) +
+Star_Wars_characters <- read.csv("00_raw_data/SW-characters.csv")
+# glimpse(Star_Wars_characters$birth_year)
+# skimr::skim(Star_Wars_characters)
+sw_clean <- janitor::clean_names(Star_Wars_characters)
+sw_clean <- sw_clean |> mutate(
+  mass = as.integer(mass),
+  gender = as.factor(gender)
+)
+# glimpse(sw_clean)
+sw_clean |>
+  count(gender) |>
+  ggplot(aes(
+    x = fct_reorder(gender, n, .desc = TRUE),
+    y = n
+  )) +
   geom_col()
-#Star_Wars_characters |> gg_miss_var()
-sw_clean |> 
-  select(-name) |> 
+# Star_Wars_characters |> gg_miss_var()
+sw_clean |>
+  select(-name) |>
   mcar_test()
+sw_clean %>% count()
+
+sw_nona <-
+  sw_clean %>%
+  select(-name) %>%
+  as.data.frame() %>%
+  simputation::impute_knn(. ~ .)
+
+sw_nona <-
+  sw_nona %>%
+  mutate(
+    height = as.numeric(height),
+    mass = as.numeric(mass)
+  )
+
+glimpse(sw_nona)
